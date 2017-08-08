2c438345d754ac47e58bcb066c530ecd0a400627
Matěj Sychra <suculent@me.com>
2017-08-08 19:20:04 +0200

    branch sync

--- fs/init.js
+++ fs/init.js
@@ -2,10 +2,10 @@ load("api_http.js");
 load("api_file.js");
 load('api_timer.js');
 
-print("Importing JSON:");
+//print("Importing JSON:");
 let thx = JSON.parse(File.read('conf5.json'));
-print("JSON:");
-print(JSON.stringify(thx));
+//print("JSON:");
+//print(JSON.stringify(thx));
 
 ///
 
@@ -14,14 +14,14 @@ let thx_connected_response = { status: "connected" };
 let thx_disconnected_response = { status: "disconnected" };
 let thx_reboot_response = { status : "rebooting" };
 let thx_update_question =
-  "{ title: \"Update Available\", body: \"There is an update available for this device. Do you want to install it now?\", type: \"actionable\", response_type: \"bool\" }";
+  { title: "Update Available", body: "There is an update available for this device. Do you want to install it now?", type: "actionable", response_type: "bool" };
 let thx_update_success =
-  "{ title: \"Update Successful\", body: \"The device has been successfully updated.\", type: \"success\" }";
+  { title: "Update Successful", body: "The device has been successfully updated.", type: "success" };
 
 ///
 
 function thinx_device_mac() {
-  return "NO:NM:OC:KE:D_"; // todo, replace with real non-mocked MAC
+  return "N0:NM:OC:KE:D1:00"; // todo, replace with real non-mocked MAC 
 }
 
 function registration_json_body() {
@@ -43,7 +43,7 @@ function registration_json_body() {
 // Hardcoded registration process
 function thinx_register() {
   //restore_device_info()
-
+  print("Registering...");
   HTTP.query({
     url: 'http://thinx.cloud:7442/device/register',
     headers: {
@@ -56,6 +56,23 @@ function thinx_register() {
     data: registration_json_body(),
     success: function(body, full_http_msg) {
       print(body);
+      
+      let json = JSON.parse(body);
+      print(JSON.stringify(json));
+      let reg = json['registration'];
+      if (reg) {
+        
+        let udid = reg['udid'];
+        if (udid) {
+          print(JSON.stringify(reg));
+          thx.THINX_UDID = udid;
+          print("Saving UDID: ");
+          print(reg['udid']);
+          let data = JSON.stringify(thx);
+          File.write(data, "conf5.json");
+        }
+      }
+      
     },
     error: function(err) {
       print(err);
@@ -64,12 +81,14 @@ function thinx_register() {
   });
 }
 
-Timer.set(5000 /* 1 sec */, true /* repeat */, function() {
+Timer.set(60000 /* 1 sec */, true /* repeat */, function() {
+  // Retry registration until UDID given
+  //if (thx.THINX_UDID === "") {
   thinx_register();
+  //}
 }, null);
 
 
-
-/*
-load('api_config.js');
-load
+Timer.set(15000 /* 1 sec */, true /* repeat */, function() {
+  thinx_register();
+}, null);
