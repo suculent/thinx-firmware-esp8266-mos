38c63eef1bf6c46f2afa07e019906bc31fc0a4df
Matěj Sychra <suculent@me.com>
2017-08-20 19:12:44 +0200

    working registration after WiFi status check

---
+++ fs/init.js
@@ -0,0 +1,165 @@
+/* jshint esversion: 6 */
+
+load("api_http.js");
+load("api_file.js");
+load('api_timer.js');
+
+//print("Importing JSON:");
+let cfg_file = File.read('conf5.json');
+let thx = JSON.parse(cfg_file);
+
+let useProxy = true;
+let thx_thinx_proxy = thx.THINX_PROXY; // should be your proxy IP otherwise Mongoose will resolve using 8.8.8.8 and fail
+
+///
+
+let thx_connected_response = {
+  status: "connected"
+};
+let thx_disconnected_response = {
+  status: "disconnected"
+};
+let thx_reboot_response = {
+  status: "rebooting"
+};
+let thx_update_question = {
+  title: "Update Available",
+  body: "There is an update available for this device. Do you want to install it now?",
+  type: "actionable",
+  response_type: "bool"
+};
+let thx_update_success = {
+  title: "Update Successful",
+  body: "The device has been successfully updated.",
+  type: "success"
+};
+
+///
+
+function thinx_device_mac() {
+  return "FA:KE:BU:TG:00:D0";
+
+  /* Returns <???-562948879850768> or crashes
+  print("Getting mac address...");
+  let get_mac_address = ffi('char * get_mac_address()');
+  print("MAC: ");
+  let result = get_mac_address();
+  print(result);
+  return result;
+  */
+}
+
+function registration_json_body() {
+  return {
+    registration: {
+      mac: thinx_device_mac(),
+      firmware: thx.THINX_FIRMWARE_VERSION,
+      commit: thx.THINX_COMMIT_ID,
+      version: thx.THINX_FIRMWARE_VERSION_SHORT,
+      checksum: thx.THINX_COMMIT_ID,
+      alias: thx.THINX_ALIAS,
+      udid: thx.THINX_UDID,
+      owner: thx.THINX_OWNER,
+      platform: 'mongoose'
+    }
+  };
+}
+
+function headers() {
+  return {
+    'Authentication': thx.THINX_API_KEY,
+    'Accept': 'application/json',
+    'Origin': 'device',
+    'Content-type': 'application/json',
+    'User-Agent': 'THiNX-Client'
+  };
+}
+
+// Hardcoded registration process
+function thinx_register() {
+  //restore_device_info()
+  print("Registering...");
+
+  let url = "http://thinx.cloud:7442/device/register";
+  if (useProxy) {
+    url = "http://" + thx_thinx_proxy + ":7442/device/register";
+    print("Trying proxy..." + url);
+  } else {
+    url = "http://207.154.230.212:7442/device/register";
+    print("Trying cloud..." + url);
+  }
+  //207.154.230.212
+  let rheaders = JSON.stringify(headers());
+  print("Request headers:" + rheaders);
+  let rbody = JSON.stringify(registration_json_body());
+  print("Request body:" + rbody);
+  // sends data but body is missing!!! why?
+  HTTP.query({
+    url: url,
+    headers: headers(),
+    data: registration_json_body(),
+    success: function(body, full_http_msg) {
+
+      if (body === "no_registration_info") {
+        print("The code is wrong. Let's fix it.");
+
+        return;
+      } else {
+        print(full_http_msg);
+      }
+
+      print("Registration response:");
+      print(body);
+
+      let json = JSON.parse(body);
+      print(JSON.stringify(json));
+      let reg = json.registration;
+      if (reg) {
+
+        //{"registration":
+        //{"status":"OK",
+        //"udid":"f8e88e40-43c8-11e7-9ad3-b7281c2b9610",
+        // "alias":"wrooom-esp32-dev",
+        // "owner":"cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12",
+        // "success":true}}
+
+        let udid = reg.udid;
+        if (udid) {
+          print(JSON.stringify(reg));
+          thx.THINX_UDID = udid;
+          print("Saving UDID: ");
+          print(reg.udid);
+          let data = JSON.stringify(thx);
+          File.write(data, "conf5.json");
+        }
+      }
+
+    },
+    error: function(err) {
+      print("Registration error:");
+      print(JSON.stringify(err));
+      useProxy = !useProxy;
+      thinx_register(useProxy);
+    },
+  });
+}
+
+// Monitor network connectivity.
+Net.setStatusEventHandler(function(ev, arg) {
+  let evs = "???";
+  if (ev === Net.STATUS_DISCONNECTED) {
+    evs = "DISCONNECTED";
+  } else if (ev === Net.STATUS_CONNECTING) {
+    evs = "CONNECTING";
+  } else if (ev === Net.STATUS_CONNECTED) {
+    evs = "CONNECTED";
+  } else if (ev === Net.STATUS_GOT_IP) {
+    evs = "GOT_IP";
+    thinx_register(useProxy);
+  }
+  print("== Net event:", ev, evs);
+}, null);
+
+
+
+///
--- src/main.c
+++ src/main.c
@@ -1,5 +1,47 @@
 #include "mgos.h"
+#include "mjs.h"
+
+static void wifi_cb(enum mgos_net_event ev,
+                    const struct mgos_net_event_data *ev_data, void *arg) {
+  if (ev_data->if_type != MGOS_NET_IF_TYPE_WIFI) return;
+  LOG(LL_INFO, ("WiFi change event: %d, arg %p", (int) ev, arg));
+}
+
+static void wifi_scan_cb(int n, struct mgos_wifi_scan_result *res, void *arg) {
+  LOG(LL_INFO, ("WiFi scan result: SSIDs %d, arg %p, results:", n, arg));
+  for (int i = 0; i < n; i++) {
+    LOG(LL_INFO, ("  SSID: %-32s, auth: %2d, channel: %2d, RSSI: %2d",
+                  res[i].ssid, res[i].auth_mode, res[i].channel, res[i].rssi));
+  }
+  LOG(LL_INFO, ("WiFi scan done."));
+}
 
 enum mgos_app_init_result mgos_app_init(void) {
+  mgos_net_add_event_handler(wifi_cb, NULL);
+  //mgos_wifi_scan(wifi_scan_cb, NULL);
   return MGOS_APP_INIT_SUCCESS;
+}
+
+
+uint8_t * mac_uin8t;
+char mac_str[64];
+
+void get_mac_address (uint8_t * mac_uin8t, char * mac_str)
+{
+    device_get_mac_address(mac_uin8t);
+    *mac_uin8t=0x5C;                //Originally informs 5E (AP MAC ADDRESS)
+    *(mac_uin8t+1)=0xCF;                            //Not really necessary
+    *(mac_uin8t+2)=0x7F;                            //Not really necessary
+    sprintf(mac_str, "%02x%02x%02x%02x%02x%02x\n", mac_uin8t[0]&0xFF,mac_uin8t[1]&0xFF,mac_uin8t[2]&0xFF,mac_uin8t[3]&0xFF,mac_uin8t[4]&0xFF,mac_uin8t[5]&0xFF);
+    puts(mac_string);
+}
+
+char * get_mac_address()
+{
+    device_get_mac_address(mac_uin8t);
+    //*mac_uin8t=0x5C;                //Originally informs 5E (AP MAC ADDRESS)
+    //*(mac_uin8t+1)=0xCF;                            //Not really necessary
+    //*(mac_uin8t+2)=0x7F;                            //Not really necessary
+    sprintf(mac_str, "%02x%02x%02x%02x%02x%02x\n", mac_uin8t[0]&0xFF,mac_uin8t[1]&0xFF,mac_uin8t[2]&0xFF,mac_uin8t[3]&0xFF,mac_uin8t[4]&0xFF,mac_uin8t[5]&0xFF);
+    return mac_str;
 }
